05851c8684b68a80ed0737ca7ce371f77aa5c92b
cooky73200 <cooky73200@gmail.com>
2021-04-13 21:43:02 +0800

    add filter

--- reviews.py
+++ reviews.py
@@ -1,5 +1,6 @@
 data = []
 count = 0
+count_1 = 0
 i = 0
 g = 0
 with open('reviews.txt', 'r') as f:
@@ -13,7 +14,12 @@ print('檔案讀取完 總共有', len(data), '筆資料')
 while i < len(data):
 	g = len(data[i]) + g
 	i = i + 1	
-print('平均資料長度為', g / len(data), '個字')	
-	
+print('平均資料長度為', g / len(data), '個字')
+new = []	
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('字串小於100的留言有', len(new), '筆')		
+
 
 	
